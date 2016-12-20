83791a91fa4e6c9ab9d6a4495114f047a5e75cab
Austin Hugenberg <austin.hugenberg@duethealth.com>
2016-12-19 20:25:51 -0500

    reading file

---
+++ app/src/main/java/com/hugey/scattools/Category.java
@@ -0,0 +1,77 @@
+package com.hugey.scattools;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by austin on 12/13/16.
+ */
+public class Category implements Parcelable {
+    @Expose
+    @SerializedName("id")
+    private String id;
+    @Expose
+    @SerializedName("category")
+    private String category;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Category(Parcel in) {
+        id = in.readString();
+        category = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(id);
+        parcel.writeString(category);
+    }
+
+    public static final Creator<Category> CREATOR = new Creator<Category>() {
+        @Override
+        public Category createFromParcel(Parcel in) {
+            return new Category(in);
+        }
+
+        @Override
+        public Category[] newArray(int size) {
+            return new Category[size];
+        }
+    };
+
+    public class CategoryViewHolder extends RecyclerView.ViewHolder {
+        private TextView mTvNumber;
+        private TextView mTvCategory;
+
+        public CategoryViewHolder(View itemView) {
+            super(itemView);
+        }
+
+
+    }
+}
--- app/src/main/java/com/hugey/scattools/ListActivity.java
+++ app/src/main/java/com/hugey/scattools/ListActivity.java
@@ -1,8 +1,22 @@
 package com.hugey.scattools;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Button;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by austin on 11/28/16.
@@ -10,8 +24,32 @@ import android.support.v7.app.AppCompatActivity;
 
 public class ListActivity extends AppCompatActivity {
 
+
+    private Button mBtnTimer;
+    private Button mBtnReset;
+    private ArrayList<Category> categories;
+    private RecyclerView mCategoryView;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        try {
+            Gson gson = new Gson();
+            JsonReader reader = new JsonReader(new FileReader("Category.json"));
+            List<Category> data = gson.fromJson(reader, Category.class);
+
+            Log.d("data asdf: " + data.size(), " size");
+        }
+        catch (FileNotFoundException fnf) {
+            Log.d("FNF ", fnf.toString());
+            finish();
+        }
     }
+
+
+
+
 }
