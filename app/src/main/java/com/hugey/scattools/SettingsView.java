569c9c26772945154fa208968d66b95e6f40bb45
Austin Hugenberg <ahugenberg9@gmail.com>
2018-02-20 09:51:21 -0500

    settings page is OK, need to implement the settings changes

--- app/src/main/java/com/hugey/scattools/SettingsView.java
+++ app/src/main/java/com/hugey/scattools/SettingsView.java
@@ -6,26 +6,50 @@ import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.util.Log;
 
 /**
  * Created by user on 12/29/17.
- */
+ *
+ * //TODO maintain a Settings object when this page goes up/down
+ * **/
 
 public class SettingsView extends PreferenceFragment implements Preference.OnPreferenceChangeListener{
 
-    private static final String KEY_TIMER = "list_preference_timer_duration";
+    private static final String KEY_SKIP = "switch_preference_skip_previous";
     private static final String KEY_ALPHABET = "list_preference_alphabet";
+    private static final String KEY_TIMER = "list_preference_timer_duration";
+    private static final String KEY_EXPIRE = "list_preference_expire_sound";
+    private static final String KEY_TICK = "switch_preference_play_tick";
+
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.settings_preferences);
 
-        Preference pref = getPreferenceManager().findPreference(KEY_ALPHABET);
+        Preference pSkip = getPreferenceManager().findPreference(KEY_SKIP);
+        Preference pAlphabet = getPreferenceManager().findPreference(KEY_ALPHABET);
+        Preference pTimer = getPreferenceManager().findPreference(KEY_TIMER);
+        Preference pExpire = getPreferenceManager().findPreference(KEY_EXPIRE);
+        Preference pTick = getPreferenceManager().findPreference(KEY_TICK);
 
-        if (pref != null) {
-            pref.setOnPreferenceChangeListener(this);
+
+        if (pAlphabet != null) {
+            pAlphabet.setOnPreferenceChangeListener(this);
+        }
+        if (pTimer != null) {
+            pTimer.setOnPreferenceChangeListener(this);
+        }
+        if (pExpire != null) {
+            pExpire.setOnPreferenceChangeListener(this);
+        }
+        if (pTick != null) {
+            pTick.setOnPreferenceChangeListener(this);
+        }
+        if (pSkip != null) {
+            pSkip.setOnPreferenceChangeListener(this);
         }
 
     }
@@ -35,10 +59,14 @@ public class SettingsView extends PreferenceFragment implements Preference.OnPre
     public boolean onPreferenceChange(Preference preference, Object o) {
         String key = preference.getKey();
 
-        Log.d("Preference String: ", key);
-
+        if (isListPreference(key)) {
+            //preference.setSummary(o.toString());
+        }
 
         return true;
+    }
 
+    private boolean isListPreference(String key) {
+        return (TextUtils.equals(key, KEY_ALPHABET) || TextUtils.equals(key, KEY_TIMER) || TextUtils.equals(key, KEY_EXPIRE));
     }
 }
